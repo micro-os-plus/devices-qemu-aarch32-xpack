b58e9f29809f1f98d9e320e5e4c22ec5b5cc29ce
Liviu Ionescu <ilg@livius.net>
2026-07-28 21:39:44 +0300

    cosmetics for #if defined(__cplusplus)

--- include/micro-os-plus/architecture-aarch32/exception-handlers.h
+++ include/micro-os-plus/architecture-aarch32/exception-handlers.h
@@ -19,7 +19,7 @@
 #if defined(__cplusplus)
 extern "C"
 {
-#endif
+#endif // defined(__cplusplus)
 
 #if defined(__ARM_ARCH_7A__)
 
@@ -42,7 +42,7 @@ extern "C"
 
 #if defined(__cplusplus)
 }
-#endif
+#endif // defined(__cplusplus)
 
 // ----------------------------------------------------------------------------
 
